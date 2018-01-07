504154b8e5eef015b6ee98186c6de8e6662e7c9f
Cl1608Ho <cl1608ho@gmail.com>
2018-01-07 16:07:40 +0100

    Added env-setup.sh for convinient environment setup (#49)
    * Added env-setup.sh for convinient environment setup
    * Fixed LIRIDIR. optional overridable

---
+++ env-setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ -n "$LIRIDIR" ]; then
+  LIRIDIR="$(realpath $LIRIDIR)"
+else
+  REPOBASE="$(realpath $(dirname ${BASH_SOURCE[0]}))"
+  LIRIDIR="$REPOBASE/.build"
+fi
+
+if [ ! -d "$LIRIDIR" ]; then
+  mkdir -p "$LIRIDIR"
+fi
+
+export LD_LIBRARY_PATH="$LIRIDIR/lib:$LD_LIBRARY_PATH"
+export XDG_DATA_DIRS="$LIRIDIR/share:/usr/local/share:/usr/share:~/.local/share:~/.local/share/flatpak/exports/share"
+export XDG_CONFIG_DIRS="$LIRIDIR/etc/xdg:/etc/xdg"
+export QT_PLUGIN_PATH="$LIRIDIR/lib/plugins"
+export QML2_IMPORT_PATH="$LIRIDIR/lib/qml:$QML2_IMPORT_PATH"
+export PKG_CONFIG_PATH="$LIRIDIR/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PATH="$LIRIDIR/bin:$PATH"
+
+if [ "$1" != "nvidia" ]; then
+  export QT_XCB_GL_INTEGRATION="xcb_egl"
+fi
