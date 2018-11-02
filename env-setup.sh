115481140902af4a66d9a78eef2b062cc7959f0b
Pier Luigi Fiorini <pierluigi.fiorini@liri.io>
2018-11-02 06:41:12 +0100

    Add CMake support
    Keep a copy of the previous version of env-setup.sh for Qbs users.
    
    Issue: #60

--- env-setup.sh
+++ env-setup.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-if [ -z "$LIRI_PREFIX" ]; then
-    LIRI_PREFIX=/usr/local
-fi
-
-if [ -z "$LIRI_CONFIG_NAME" ]; then
-    LIRI_CONFIG_NAME="qtc_Desktop_Debug"
-fi
-
 if [ -n "$LIRIDIR" ]; then
     LIRIDIR="$(realpath $LIRIDIR)"
 else
     REPOBASE="$(realpath $(dirname ${BASH_SOURCE[0]}))"
-    LIRIDIR="$REPOBASE/.build/$LIRI_CONFIG_NAME/install-root"
+    LIRIDIR="$REPOBASE/.build/install-root"
 fi
 
 if [ ! -d "$LIRIDIR" ]; then
     mkdir -p "$LIRIDIR"
 fi
 
-export LD_LIBRARY_PATH="$LIRIDIR/$LIRI_PREFIX/lib:$LD_LIBRARY_PATH"
-export XDG_DATA_DIRS="$LIRIDIR/$LIRI_PREFIX/share:/usr/local/share:/usr/share:~/.local/share:~/.local/share/flatpak/exports/share"
-export XDG_CONFIG_DIRS="$LIRIDIR/etc/xdg:/etc/xdg"
-export QT_PLUGIN_PATH="$LIRIDIR/$LIRI_PREFIX/lib/plugins"
-export QML2_IMPORT_PATH="$LIRIDIR/$LIRI_PREFIX/lib/qml:$QML2_IMPORT_PATH"
-export PKG_CONFIG_PATH="$LIRIDIR/$LIRI_PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH"
-export PATH="$LIRIDIR/$LIRI_PREFIX/bin:$PATH"
+if [ -z "$XDG_DATA_DIRS" ]; then
+    XDG_DATA_DIRS="/usr/local/share:/usr/share"
+fi
+
+if [ -z "$XDG_CONFIG_DIRS" ]; then
+    XDG_CONFIG_DIRS="/etc/xdg"
+fi
+
+export LD_LIBRARY_PATH="$LIRIDIR/lib:$LD_LIBRARY_PATH"
+export XDG_DATA_DIRS="$LIRIDIR/share:$XDG_DATA_DIRS"
+export XDG_CONFIG_DIRS="$LIRIDIR/etc/xdg:$XDG_CONFIG_DIRS"
+export QT_PLUGIN_PATH="$LIRIDIR/lib/plugins"
+export QML2_IMPORT_PATH="$LIRIDIR/lib/qml:$QML2_IMPORT_PATH"
+export PKG_CONFIG_PATH="$LIRIDIR/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PATH="$LIRIDIR/bin:$PATH"
 
 if [ "$1" == "nvidia" ]; then
     export QT_XCB_GL_INTEGRATION="xcb_glx"
